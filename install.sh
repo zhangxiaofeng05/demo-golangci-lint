aece725e54ecab255a53fd8585ce80c403732ed5
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-02-27 15:57:47 +0800

    feat(install): add check

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# Add the following content to the makefile
-echo "start appent lint to Makefile"
-curl https://raw.githubusercontent.com/zhangxiaofeng05/demo-golangci-lint/main/Makefile >> Makefile
+if [ -e ".hooks/pre-commit" ]; then
+	echo ".hooks/pre-commit exist!!!"
+	make git_hook
+	echo "finish run make git_hook, enjoy it!"
+	exit 1
+fi
+
+echo "** start init"
 # get pre-commit
 echo "start get pre-commit"
 wget -P .hooks https://raw.githubusercontent.com/zhangxiaofeng05/demo-golangci-lint/main/.hooks/pre-commit
+# Add the following content to the makefile
+echo "start appent lint to Makefile"
+curl https://raw.githubusercontent.com/zhangxiaofeng05/demo-golangci-lint/main/Makefile >> Makefile
 # get golangci-lint config file
 echo "start get golangci-lint config file"
 wget https://raw.githubusercontent.com/zhangxiaofeng05/demo-golangci-lint/main/.golangci.yml
@@ -16,4 +24,4 @@ chmod +x .hooks/pre-commit
 echo "start put pre-commit to .git/hooks"
 make git_hook
 
-echo "ok, let us enjoy it!"
+echo "** ok, let us enjoy it!"
